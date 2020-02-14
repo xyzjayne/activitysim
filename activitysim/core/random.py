correct lognormal calculations and add functions to calculation lognormal location and scale

activitysim/core/random.py
@@ -195,9 +195,9 @@ class SimpleChannel(object):
         """
 
         # assert no dupes
-        #bug
+        # bug
         if len(df.index.unique()) < len(df.index):
-            #print(df)
+            # print(df)
             bug
 
         assert len(df.index.unique()) == len(df.index)
@@ -690,8 +690,8 @@ class Random(object):
         """
 
         if broadcast:
-            # Note that the mean and standard deviation are not the values for the distribution itself,
-            # but of the underlying normal distribution it is derived from.
+            # Note that the mean and standard deviation are not the values for the distribution
+            # itself, but of the underlying normal distribution it is derived from.
             rands = self.normal_for_df(df, mu=mu, sigma=sigma, broadcast=True)
             rands = np.exp(rands)
         else:
@@ -747,3 +747,41 @@ class Random(object):
         choices = channel.choice_for_df(df, self.step_name, a, size, replace)
         t0 = print_elapsed_time("choice_for_df for %s rows" % len(df.index), t0, debug=True)
         return choices
+
+    def calculate_location(self, mean, std_dev):
+        """
+        Calculate the lognormal distribution location given the mean and standard
+        deviation of the distribution according to the formula
+
+        scale = sqrt(ln(1 + std_dev^2/mean^2))
+
+        Parameters
+        ----------
+        mean : float
+        std_dev : float
+
+        Returns
+        -------
+        location : lognormal distribution location
+        """
+        location = np.log(mean / (np.sqrt(1 + ((std_dev * std_dev) / (mean * mean)))))
+        return(location)
+
+    def calculate_scale(self, mean, std_dev):
+        """
+        Calculate the lognormal distribution scale given the mean and standard
+        deviation of the distribution according to the formula
+
+        location = ln(mean/sqrt(1 + std_dev^2/mean^2))
+
+        Parameters
+        ----------
+        mean : float
+        std_dev : float
+
+        Returns
+        -------
+        scale : lognormal distribution scale
+        """
+        scale = np.sqrt(np.log(1 + ((std_dev * std_dev) / (mean * mean))))
+        return(scale)